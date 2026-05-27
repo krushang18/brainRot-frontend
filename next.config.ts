99aaf2b256034fa58f6cb631620cb52c739c0def
Aarya1402 <patelaarya1402@gmail.com>
2026-05-27 11:44:09 +0530

    chore: add brainrot-frontend.home.arpa to allowedDevOrigins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  allowedDevOrigins: ['192.168.24.68', '192.168.24.136','192.168.24.75', 'localhost'],
+  allowedDevOrigins: ['192.168.24.68', '192.168.24.136', '192.168.24.75', 'localhost', 'brainrot-frontend.home.arpa'],
 };
 
 export default nextConfig;
